35155429a304cae7e96b875e4d6c52861a648c7c
Vladimir Kulakov <vkulakov@gmx.com>
2024-04-30 14:44:10 +0300

    minor fix

--- src/aoc2016day03.py
+++ src/aoc2016day03.py
@@ -45,11 +45,11 @@ def count_valid_triangles_variant_2(file):
 
 def main():
     """ main function """
-    with open(r"puzzles\aoc2016day03_data.txt", encoding="utf-8") as file:
+    with open(r"puzzles/aoc2016day03_data.txt", encoding="utf-8") as file:
         print(
             f"It is possible {count_valid_triangles_variant_1(file)} triangles using variant 1.")
 
-    with open(r"puzzles\aoc2016day03_data.txt", encoding="utf-8") as file:
+    with open(r"puzzles/aoc2016day03_data.txt", encoding="utf-8") as file:
         print(
             f"It is possible {count_valid_triangles_variant_2(file)} triangles using variant 2.")
 
