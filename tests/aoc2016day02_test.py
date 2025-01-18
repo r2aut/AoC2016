24da650831e2098edfaf468732c75d1a86773ff5
Vladimir Kulakov <vkulakov@gmx.com>
2025-01-18 10:15:48 +0300

    day02 refactored

--- tests/aoc2016day02_test.py
+++ tests/aoc2016day02_test.py
@@ -2,19 +2,33 @@
 
 import io
 
-from src.aoc2016day02 import get_digits
+from src.aoc2016day02 import Position, get_digits
 
-def test_get_digits():
-    """ get_gigits function unit test """
 
-    test_keypad = ( ('1','2','3','0'),
-                    ('4','5','6','0'),
-                    ('7','8','9','0'),
-                    ('0','0','0','0')
-                  )
+def test_get_digits1():
+    """Get_gigits function unit test for variant 1"""
+
+    test_keypad1 = (("1", "2", "3", "0"), ("4", "5", "6", "0"), ("7", "8", "9", "0"), ("0", "0", "0", "0"))
 
     test_str = "ULL\nRRDDD\nLURDL\nUUUUD"
 
     with io.StringIO(test_str) as file:
-        gen = get_digits(test_keypad, (1,1), file)
-        assert "".join((gen)) == "1985"
+        dig = get_digits(test_keypad1, Position(1, 1), file)
+        assert dig == "1985"
+
+
+def test_get_digits2():
+    """Get_gigits function unit test for variant 2"""
+
+    test_keypad2 = (
+        ("0", "0", "1", "0", "0"),
+        ("0", "2", "3", "4", "0"),
+        ("5", "6", "7", "8", "9"),
+        ("0", "A", "B", "C", "0"),
+        ("0", "0", "D", "0", "0"),
+    )
+    test_str = "ULL\nRRDDD\nLURDL\nUUUUD"
+
+    with io.StringIO(test_str) as file:
+        dig = get_digits(test_keypad2, Position(0, 2), file)
+        assert dig == "5DB3"
