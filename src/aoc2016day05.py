7eecb9b6d9976b6f8dc567c25f94ececa0a0428f
Vladimir Kulakov <vkulakov@gmx.com>
2025-10-22 19:15:14 +0300

    Solve day05

---
+++ src/aoc2016day05.py
@@ -0,0 +1,83 @@
+"""Day 5: How About a Nice Game of Chess."""
+
+from collections.abc import Generator
+from hashlib import md5
+from pathlib import Path
+from typing import Any, NoReturn
+
+from rich.console import Console
+from rich.progress import BarColumn, Progress, TaskProgressColumn, TextColumn, TimeElapsedColumn
+
+
+def hash_gen(door_id: str, pattern: str) -> Generator[str, Any, NoReturn]:
+    """Generate valid hashes."""
+    index = 0
+    while True:
+        door_str = door_id + str(index)
+        digest = md5(door_str.encode("utf-8")).hexdigest()  # noqa: S324
+        if digest.startswith(pattern):
+            yield digest
+        index += 1
+
+
+def part_1(door_id: str, pattern: str, pass_len: int, /, progress: Progress | None = None) -> str:
+    """Get the password for Part One."""
+    ch_index = len(pattern)
+    if progress:
+        task1 = progress.add_task("Processing Part One ...", total=pass_len)
+    hashes = hash_gen(door_id, pattern)
+    result = []
+    for _ in range(pass_len):
+        ch = next(hashes)[ch_index]
+        result.append(ch)
+        if progress:
+            progress.update(task1, advance=1)  # pyright: ignore[reportPossiblyUnboundVariable]
+    return "".join(result)
+
+
+def part_2(door_id: str, pattern: str, pass_len: int, /, progress: Progress | None = None) -> str:
+    """Get the password for Part Two."""
+    if progress:
+        task2 = progress.add_task("Processing Part Two ...", total=pass_len)
+    pos_index = len(pattern)
+    ch_index = len(pattern) + 1
+    hashes = hash_gen(door_id, pattern)
+    result: list = [None] * pass_len
+    pos_char_list = [str(ch) for ch in range(pass_len)]
+    while not all(result):
+        c = next(hashes)
+        pos_char = c[pos_index]
+        if pos_char in pos_char_list:
+            pos = int(pos_char)
+            ch = c[ch_index]
+            if pos < 8 and not result[pos]:  # noqa: PLR2004
+                result[pos] = ch
+                if progress:
+                    progress.update(task2, advance=1)  # pyright: ignore[reportPossiblyUnboundVariable]
+    return "".join(result)
+
+
+def main():  # noqa: ANN201, D103
+    with Path(r"puzzles/aoc2016day05_data.txt").open(encoding="utf-8") as file:
+        door_id = file.readline().strip()
+
+    rc = Console()
+
+    door_id = "abc"
+    pattern = "00000"
+
+    with Progress(  # show progress bar to display process
+        TextColumn("[progress.description]{task.description}"),
+        BarColumn(),
+        TaskProgressColumn(),
+        TimeElapsedColumn(),
+    ) as progress:
+        res_1 = part_1(door_id, pattern, 8, progress=progress)
+        res_2 = part_2(door_id, pattern, 8, progress=progress)
+
+    rc.print(f"[cyan](Part One)[/cyan] The password is [green bold]{res_1}[/green bold]")
+    rc.print(f"[cyan](Part Two)[/cyan] The password is [green bold]{res_2}[/green bold]")
+
+
+if __name__ == "__main__":
+    main()
---
+++ tests/aoc2016day05_test.py
@@ -0,0 +1,23 @@
+"""Day 5: How About a Nice Game of Chess."""
+
+# ruff: noqa: S101, ANN201
+
+from src.aoc2016day05 import hash_gen, part_1, part_2
+
+
+def test_hash_gen():
+    """Test hash generator."""
+    hasher = hash_gen("abc", "00000")
+    assert next(hasher) == "00000155f8105dff7f56ee10fa9b9abd"
+    assert next(hasher) == "000008f82c5b3924a1ecbebf60344e00"
+    assert next(hasher) == "00000f9a2c309875e05c5a5d09f1b8c4"
+
+
+def test_part_1():
+    """Test Part One."""
+    assert part_1("abc", "00000", 8) == "18f47a30"
+
+
+def test_part_2():
+    """Test Part Two."""
+    assert part_2("abc", "00000", 8) == "05ace8e3"
