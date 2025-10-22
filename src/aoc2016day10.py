e39e535c8041e69beb9ae4353609c072726bf997
Vladimir Kulakov <vkulakov@gmx.com>
2025-10-22 19:19:53 +0300

    Refactor day10

--- src/aoc2016day10.py
+++ src/aoc2016day10.py
@@ -1,116 +1,116 @@
-""" Day 10: Balance Bots """
+"""Day 10: Balance Bots."""
 
-import io
-from multiprocessing import Value
+# ruff: noqa: D101, D105, D107, ANN204
+
+from io import TextIOBase
+from pathlib import Path
+
+from rich.console import Console
 
 
 class Chip:
-    value = 0
-
-    def __init__(self, value) -> None:
-        self.value = value
+    def __init__(self, value: int) -> None:
+        self.value = value  # microchip's number
 
     def __repr__(self) -> str:
         return str(self.value)
 
-    def __lt__(self, obj):
+    def __lt__(self, obj: "Chip"):
         return self.value < obj.value
 
 
 class OutputBin:
-    number = 0
-    bin = None
+    def __init__(self, number: int) -> None:
+        self.number = number  # bin number
+        self.bin: list[Chip] = []
 
-    def __init__(self, number) -> None:
-        self.number = number
-        self.bin = list()
-
-    def __getitem__(self, index):
-        return bin[index]
+    def __getitem__(self, index: int):
+        return self.bin[index]
 
     def __repr__(self) -> str:
         return str(self.number) + ":" + str(self.bin)
 
-    def __iadd__(self, chip):
+    def __iadd__(self, chip: Chip):
         self.bin.append(chip)
 
-    def push(self, chip):
+    def push(self, chip: Chip) -> None:
+        """Push the chip into the bin."""
         self.bin.append(chip)
 
 
 class Bot:
-    number = 0
-    chips = None
-    condition = None
-
-    def __init__(self, number) -> None:
-        self.number = number
-        self.chips = list()
-
-    def pop_low_chip(self) -> Chip:
-        return self.chips.pop(0)
-
-    def pop_high_chip(self) -> Chip:
-        return self.chips.pop(-1)
-
-    def __iadd__(self, chip):
-        self.chips.append(chip)
-        self.chips.sort()
-
-    def push(self, chip):
-        self.chips.append(chip)
-        self.chips.sort()
+    def __init__(self, number: int) -> None:
+        self.number = number  # bot's number
+        self.chips: list[Chip] = []
 
     def __repr__(self) -> str:
         return str(self.number) + ":" + str(self.chips)
 
-    def __contains__(self, item):
-        for i in self.chips:
-            if i.value == item.value:
-                return True
-        return False
+    def __contains__(self, item: Chip):
+        return any(i.value == item.value for i in self.chips)
 
-    def compare_chips(self, a, b):
+    def __iadd__(self, chip: Chip):
+        self.chips.append(chip)
+        self.chips.sort()
+
+    def push(self, chip: Chip) -> None:
+        """Put chip into bot's pocket."""
+        self.chips.append(chip)
+        self.chips.sort()
+
+    def pop_low_chip(self) -> Chip:
+        """Get the first chip from the pocket."""
+        return self.chips.pop(0)
+
+    def pop_high_chip(self) -> Chip:
+        """Get the last chip from the pocket."""
+        return self.chips.pop(-1)
+
+    def compare_chips(self, a: int, b: int) -> bool:
+        """Compare chips."""
         if a > b:
             a, b = b, a
         if not len(self.chips):
             return False
-        if self.chips[0].value == a and self.chips[-1].value == b:
-            return True
-        return False
+        return self.chips[0].value == a and self.chips[-1].value == b
 
 
 class Bots:
-    bots = dict()
-
-    def __getitem__(self, index):
-        if index in self.bots:
-            return self.bots[index]
-        else:
-            bot = Bot(index)
-            self.bots[index] = bot
-            return bot
+    def __init__(self) -> None:
+        self.bots: dict[int, Bot] = {}
 
     def __repr__(self) -> str:
         return str(self.bots.values())
 
+    def __getitem__(self, index: int):
+        if index in self.bots:
+            return self.bots[index]
+        bot = Bot(index)
+        self.bots[index] = bot
+        return bot
+
 
 class OutputBins:
-    bins = dict()
-
-    def __getitem__(self, index):
-        if index in self.bins:
-            return self.bins[index]
-        else:
-            bin = OutputBin(index)
-            self.bins[index] = bin
-            return bin
+    def __init__(self) -> None:
+        self.bins: dict[int, OutputBin] = {}
 
     def __repr__(self) -> str:
         return "Output bins " + str(self.bins.values())
 
+    def __getitem__(self, index: int):
+        if index in self.bins:
+            return self.bins[index]
+        output_bin = OutputBin(index)
+        self.bins[index] = output_bin
+        return output_bin
 
-def process(program_file, compared_chips, cycle_quantity=50):
+
+def process(  # noqa: C901
+    program_file: TextIOBase,
+    compared_chips: tuple[int, int],
+    cycle_quantity: int = 50,
+) -> tuple[int | None, int]:
+    """Process."""
     output_bins = OutputBins()
     bots = Bots()
 
@@ -120,13 +120,10 @@ def process(program_file, compared_chips, cycle_quantity=50):
 
     cycle_number = cycle_quantity
     while cycle_number:
-
         for line in lines:
             parts = line.strip().split()
 
-            assert (
-                parts[0] == "value" or parts[0] == "bot"
-            ), "Bad value in bot's instruction"
+            # assert parts[0] == "value" or parts[0] == "bot", "Bad value in bot's instruction"
             if parts[0] == "value":
                 chip = Chip(int(parts[1]))
                 bot_num = int(parts[5])
@@ -134,12 +131,10 @@ def process(program_file, compared_chips, cycle_quantity=50):
                     bots[bot_num].push(chip)
             elif parts[0] == "bot":
                 bot_num = int(parts[1])
-                if len(bots[bot_num].chips) < 2:
+                if len(bots[bot_num].chips) < 2:  # noqa: PLR2004
                     continue
 
-                assert (
-                    parts[5] == "bot" or parts[5] == "output"
-                ), "Bad value in bot's instruction"
+                # assert parts[5] == "bot" or parts[5] == "output", "Bad value in bot's instruction"
                 if parts[5] == "bot":
                     dest_bot_num = int(parts[6])
                     bots[dest_bot_num].push(bots[bot_num].pop_low_chip())
@@ -147,9 +142,7 @@ def process(program_file, compared_chips, cycle_quantity=50):
                     dest_bin_num = int(parts[6])
                     output_bins[dest_bin_num].push(bots[bot_num].pop_low_chip())
 
-                assert (
-                    parts[10] == "bot" or parts[10] == "output"
-                ), "Bad value in bot's instruction"
+                # assert parts[10] == "bot" or parts[10] == "output", "Bad value in bot's instruction"
                 if parts[10] == "bot":
                     dest_bot_num = int(parts[11])
                     bots[dest_bot_num].push(bots[bot_num].pop_high_chip())
@@ -164,19 +157,18 @@ def process(program_file, compared_chips, cycle_quantity=50):
         cycle_number -= 1
 
     return (
-        comp_results.number,
-        output_bins.bins[0].bin.pop().value
-        * output_bins.bins[1].bin.pop().value
-        * output_bins.bins[2].bin.pop().value,
+        comp_results.number if comp_results else None,
+        output_bins.bins[0].bin.pop().value * output_bins.bins[1].bin.pop().value * output_bins.bins[2].bin.pop().value,
     )
 
 
-def main():
-
-    with open("puzzles/aoc2016day10_data.txt", encoding="utf-8") as file:
+def main():  # noqa: ANN201, D103
+    with Path("puzzles/aoc2016day10_data.txt").open(encoding="utf-8") as file:
         bot, mult = process(file, (17, 61), 100)
-        print(f"The number of the bot is {bot}")
-        print(f"The multiplication of values is {mult}")
+
+        rc = Console()
+        rc.print(f"[cyan](Part One)[/cyan] The number of the bot is [green]{bot}[/green]")
+        rc.print(f"[cyan](Part Two)[/cyan] The multiplication of values is [green]{mult}[/green]")
 
 
 if __name__ == "__main__":
--- tests/aoc2016day10_test.py
+++ tests/aoc2016day10_test.py
@@ -1,8 +1,10 @@
-""" Day 9: Explosives in Cyberspace unit tests """
+"""Day 9: Explosives in Cyberspace unit tests."""
 
-import io
+# ruff: noqa: S101, ANN201, PLR2004
+
+from io import StringIO
+
 from src.aoc2016day10 import process
-
 
 TEST_DATA = """value 5 goes to bot 2
 bot 2 gives low to bot 1 and high to bot 0
@@ -14,9 +16,8 @@ value 2 goes to bot 2
 
 
 def test_process():
-    """ function process unit test """
-
-    with io.StringIO(TEST_DATA) as file:
-        bot, mult = process(file, (2,5))
+    """Test process function."""
+    with StringIO(TEST_DATA) as file:
+        bot, mult = process(file, (2, 5))
         assert bot == 2
         assert mult == 30
